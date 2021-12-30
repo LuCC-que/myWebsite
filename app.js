d1871775f2adc021f8205309f824155db6909f7c
LuCC-que <chen1968493689@users.github.com>
2021-12-29 20:23:59 -0500

    fixed a stupid bug

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const { storage } = require("./cloudinary/index");
 const upload = multer({ storage });
 const { vetloggedIn, vetAuthor } = require("./middlewares");
 //process.env.DB_URL;
-const dbURL = "mongodb://localhost:27017/projects";
+const dbURL = process.env.DB_URL || "mongodb://localhost:27017/projects";
 console.log(dbURL);
 //"mongodb://localhost:27017/projects"
 
